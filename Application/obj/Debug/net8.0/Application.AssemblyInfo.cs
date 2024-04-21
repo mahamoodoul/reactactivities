ce2095b1e742aef37745a81dad88eceb41be77c2
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-04-22 00:16:06 +0200

    added activities create and delete option

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7161acba0b6f7637699504e8e1ec4ef2f2ad0f73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ff8c3b2665a5b5710e4e49b00f42e6453953b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
