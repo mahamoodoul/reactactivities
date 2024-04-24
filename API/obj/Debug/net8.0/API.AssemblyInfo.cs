d07b5096a856535b087acaf3d5db22c194cd2db9
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-04-24 15:14:33 +0200

    added api mange file

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792e456a64598f7a2197860f9afe083d5abb69d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d0a24337780c385079aed189844b5bfeb953169")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
