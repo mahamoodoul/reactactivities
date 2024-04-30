86592c093ab4c35405c9fcf2d2f5ff6c82e50deb
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-04-30 11:11:10 +0200

    added jwt autheticatiion with local storage and add auth

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c91dd123897641c3f69ffb70cb252ddc0770a798")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f2cc84aa480b48caaf62927b916a679f3c3a84")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
