2926ccd0ea42d8b599173fd976cf3ea2996d9da3
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-05-02 23:29:49 +0200

    added profile card component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620f9e92199e573728b17d3fa40b173756e6f866")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9b5e29a8b2150e052dbc04e18207cede551bbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
