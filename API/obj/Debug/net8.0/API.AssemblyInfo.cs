198b3c41f56d55577ef0abf40cd1ea1ede873564
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-05-04 00:03:34 +0200

    added add photo controller with cloud

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444b6475799cce26c3cf6b7b697c0bb9e019c0ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae792727024579210baa78f6e3e704aed19e8181")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
