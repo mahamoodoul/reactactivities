ae792727024579210baa78f6e3e704aed19e8181
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-05-03 11:03:46 +0200

    added photo controller

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb007f7a84a705ce34a81b88e8cfdd6ca45c3458")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444b6475799cce26c3cf6b7b697c0bb9e019c0ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
