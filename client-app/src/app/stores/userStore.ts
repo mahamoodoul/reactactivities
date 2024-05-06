f67c5e55f5c7439ed0db2b08f5d50848b8681fc1
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-05-06 13:44:42 +0200

    added photo dynamic options

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -54,4 +54,13 @@ export default class UserStore {
             console.log(error);
         }
     }
+
+
+    setImage = (image: string) => {
+        if (this.user) this.user.image = image;
+    }
+
+    setUserPhoto = (url: string) => {
+        if (this.user) this.user.image = url;
+    }
 }
