85278eb2ead8f1ab656aad6e36a5f94b4fdb62a0
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-05-08 14:53:07 +0200

    added about profile component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67c5e55f5c7439ed0db2b08f5d50848b8681fc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf5d8375cf393d311896aa86da830020ba0ab2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
